d1ef12e0f11d10b5e80006876ad4d9ac8f0ef854
20prince12 <46424040+20prince12@users.noreply.github.com>
2020-02-17 12:40:28 +0530

    T

--- getResults.py
+++ getResults.py
@@ -47,6 +47,6 @@ class getResultData:
             self.totalcredits += int(self.creditsData[x])
         for x in self.marksData:
             if (points[self.marksData[x]] == 0):
-            return 0
+              return 0
             cgpa += points[self.marksData[x]] * int(self.creditsData[x]) / self.totalcredits
         self.cgpa=format(cgpa,'.2f')
