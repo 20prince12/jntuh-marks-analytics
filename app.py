5275c4d77cf681af952108ab0c58177373bd81e2
20prince12 <46424040+20prince12@users.noreply.github.com>
2020-02-21 21:55:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,6 @@ app = Flask(__name__)
 
 
 
-@app.route('/calculate', methods=['GET','POST'])
-def calculate():
-    if request.method == 'POST':
-        roll=request.form.get('roll')
-        try:
-            student=getResults.getResultData(roll)
-            print(student.cgpa)
-            print(student.creditsData)
-            print(student.personalData)
-            print(student.marksData)
-        except:
-            return render_template('cgpa.html', x='Server Error or Invalid HT')
-        return render_template('cgpa.html',x=student.cgpa,y=student.backlogs)
-    else:
-        return render_template('cgpa.html')
-
 @app.route('/')
 def home():
     return render_template('home.html')
