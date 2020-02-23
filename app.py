27159324e745a9118830834f2e1596c9bd9cc36d
20prince12 <princeptl123456@gmail.com>
2020-02-23 19:57:46 +0530

    test

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-import getResults;
+from flask import Flask, render_template,request
+import getResults,pyodbc
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+server = 'resultanalytics.database.windows.net'
+database = 'asm'
+username = 'asm'
+password = 'Jalicno123'
+driver= '{ODBC Driver 13 for SQL Server}'
+cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/getlist',methods=['GET','POST'])
 def index():
+
     if request.method=="POST":
         a=request.form.get('start').upper()
         b=request.form.get('end').upper()
+        res=request.form.get('result').split(".")
         cgpalist=[]
         personalDatalist=[]
         marksDatalist=[]
@@ -35,45 +51,65 @@ def index():
 
         start = test(s1)
         end = test(s2)
-        print(start)
-        print(end)
+        #print(start)
+        #print(end)
+        if(end-start<0 or end-start>180):
+            return render_template('home.html')
         for i in range(start, end+1):
             try:
                 if (i < 10):
                     ht=rollno + '0' + str(i)
-                    student = getResults.getResultData(ht)
                 elif (i < 100):
                     ht=rollno + str(i)
-                    student = getResults.getResultData(ht)
                 elif i > 99 and i < 110:
                     ht=rollno + 'A' + str(i - 100)
-                    student = getResults.getResultData(ht)
                 elif i > 109 and i < 120:
                     ht=rollno + 'B' + str(i - 110)
-                    student = getResults.getResultData(ht)
                 elif i > 119 and i < 130:
                     ht=rollno + 'C' + str(i - 120)
-                    student = getResults.getResultData(ht)
                 elif i > 129 and i < 140:
                     ht=rollno + 'D' + str(i - 130)
-                    student = getResults.getResultData(ht)
                 elif i > 139 and i < 150:
                     ht=rollno + 'E' + str(i - 140)
-                    student = getResults.getResultData(ht)
                 elif i > 149 and i < 160:
                     ht=rollno + 'F' + str(i - 150)
-                    student = getResults.getResultData(ht)
                 elif i > 159 and i < 170:
                     ht=rollno + 'G' + str(i - 160)
-                    student = getResults.getResultData(ht)
                 elif i > 169 and i < 180:
                     ht=rollno + 'H' + str(i - 170)
-                    student = getResults.getResultData(ht)
-                cgpalist.append(student.cgpa)
-                personalDatalist.append(student.personalData)
-                marksDatalist.append(student.marksData)
+                id=ht+res[0]+res[1]
+                curs =cnxn.cursor()
+                if curs.execute("SELECT * FROM badhtno WHERE id='%s'" % id) and curs.fetchone():
+                    #print(f"{id}  not found")
+                    continue
+                elif curs.execute("SELECT * FROM data WHERE id='%s'" % id) and curs.fetchone():
+                    #print(id)
+                    curs.execute("SELECT * FROM data WHERE id='%s'" % id)
+                    dict = curs.fetchone()
+                    xcgpa = float(dict[1])
+                    xpersonalData = eval(dict[2])
+                    xmarksData = eval(dict[3])
+
+                else:
+                    print("test")
+                    student = getResults.getResultData(ht, res[0], res[1])
+                    kcgpa = str(student.cgpa)
+                    kpersonalData = str(student.personalData)
+                    kmarksData = str(student.marksData)
+                    curs.execute("INSERT INTO data(id, cgpa, personalData, marksData) VALUES(?,?,?,?)",
+                                 (id, kcgpa, kpersonalData, kmarksData))
+                    cnxn.commit()
+                    xcgpa = student.cgpa
+                    xpersonalData = student.personalData
+                    xmarksData = student.marksData
+                cgpalist.append(xcgpa)
+                personalDatalist.append(xpersonalData)
+                marksDatalist.append(xmarksData)
             except:
-                print("none found")
+                #print(f"{id}  not found2")
+                curs.execute("INSERT INTO badhtno(id) VALUES('%s')" % id)
+                cnxn.commit()
+
         #print(cgpalist)
         #print(personalDatalist)
         #print(marksDatalist)
@@ -97,11 +133,16 @@ def index():
             for k in y:
                 tableData[count].append(y[k])
             count += 1
-        print(tableData)
-        print(tableHead)
+        #print(tableData)
+        #print(tableHead)
         failed=cgpalist.count(0)
         passed=len(cgpalist)-failed
-        print(passed)
-        print(failed)
-        return render_template('getlist.html',cgpalist=cgpalist,tableHead=tableHead,tableData=tableData,tableheadLen=len(tableHead),tableDatalen=len(tableData))
-    return render_template('getlist.html')
+        #print(passed)
+        #print(failed)
+        passper=format(((passed/(passed+failed))*100),'.2f')
+        failper=format(((failed/(passed+failed))*100),'.2f')
+        #print(passper)
+        #print(failper)
+
+        return render_template('getlist.html',cgpalist=cgpalist,tableHead=tableHead,tableData=tableData,tableheadLen=len(tableHead),tableDatalen=len(tableData),passper=passper,failper=failper)
+    return render_template('home.html')
--- getResults.py
+++ getResults.py
@@ -3,47 +3,43 @@ import bs4,requests;
 
 class getResultData:
 
-    def __init__(self,rollno):
+    def __init__(self,rollno,code,batch):
         self.backlogs=0
-        originalData=requests.get('http://epayments.jntuh.ac.in/results/resultAction?degree=btech&examCode=1387&etype=r16&result=null&grad=null&type=grade16&htno='+rollno.upper());
+        originalData=requests.get(f'http://epayments.jntuh.ac.in/results/resultAction?degree=btech&examCode={code}&etype=r{batch}&result=null&grad=null&type=grade{batch}&htno='+rollno.upper());
+
         parsedData=bs4.BeautifulSoup(originalData.text,'html.parser')
         savedData=[]
         for x in parsedData.find_all('td'):
             savedData.append(str(x).replace('<td>','').replace('<b>','').replace('</b>','').replace('<h4>','').replace('</td>','').replace('</h4',''))
-
+        savedData=savedData[0:48]
         personalData={savedData[0]:savedData[1],
                     savedData[2]:savedData[3],
                     savedData[4]:savedData[5],
                     savedData[6]:savedData[7]
                   }
-        marksData={savedData[13]:savedData[14],
-                    savedData[17]:savedData[18],
-                    savedData[21]:savedData[22],
-                    savedData[25]:savedData[26],
-                    savedData[29]:savedData[30],
-                    savedData[33]:savedData[34],
-                    savedData[37]:savedData[38],
-                    savedData[41]:savedData[42],
-                    savedData[45]:savedData[46]
-                    }
-        creditsData={savedData[13]:savedData[15],
-                    savedData[17]:savedData[19],
-                    savedData[21]:savedData[23],
-                    savedData[25]:savedData[27],
-                    savedData[29]:savedData[31],
-                    savedData[33]:savedData[35],
-                    savedData[37]:savedData[39],
-                    savedData[41]:savedData[43],
-                    savedData[45]:savedData[47]
-                    }
-        self.creditsData=creditsData
-        self.marksData=marksData
-        self.personalData=personalData
+        #print(savedData)
+        personalData = {savedData[0]: savedData[1],
+                        savedData[2]: savedData[3],
+                        savedData[4]: savedData[5],
+                        savedData[6]: savedData[7]
+                        }
+
+        marksData = {}
+        creditsData = {}
+        print(len(savedData))
+        for x in range(13,46,4):
+            marksData[savedData[x]]=savedData[x+1]
+            creditsData[savedData[x]]=savedData[x+2]
+        self.creditsData = creditsData
+        self.marksData = marksData
+        self.personalData = personalData
+        #print(marksData)
+        #print(creditsData)
         cgpa = 0
         points = {'O': 10, 'A+': 9, 'A': 8, 'B+': 7, 'B': 6, 'C': 5, 'F': 0}
-        self.totalcredits = 24;
-        #for x in self.creditsData:
-        #    self.totalcredits += int(self.creditsData[x])
+        self.totalcredits = 0;
+        for x in self.creditsData:
+            self.totalcredits += int(self.creditsData[x])
         for x in self.marksData:
             if (points[self.marksData[x]] == 0):
                 self.backlogs+=1
